a737b6223a42e9f8db2d1e3138c1e0c4c3dc7a00
Manuel De Leon <manuel.deleon@pearson.com>
2015-01-29 15:47:35 -0700

    use tracker

--- package.js
+++ package.js
@@ -1,13 +1,13 @@
 Package.describe({
   name: "manuel:reactivearray",
   summary: "Reactive Array for Meteor",
-  version: "1.0.1",
+  version: "1.0.2",
   git: "https://github.com/ManuelDeLeon/ReactiveArray"
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('METEOR@1.0');
-    api.use('coffeescript');
+    api.use(['coffeescript', 'tracker']);
     api.addFiles(['ReactiveArray.coffee'], 'client');
     api.export('ReactiveArray');
 });
